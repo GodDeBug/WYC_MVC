71d87541dbd139a139bc7ed61bd0d8698cf11a1f
Administrator <Administrator@6-206-A54>
2019-05-16 09:01:51 +0800

    添加 .gitignore 和 .gitattributes。

---
+++ wyc_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wyc_MVC.Startup))]
+namespace wyc_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
